136c37a475da3b9ca185635dea62267d2c29e2f5
Maximilian Meinhardt <maximilian.meinhardt@stud.h-da.de>
2025-01-26 16:11:52 +0100

    added spawner with random timer

--- Assets/_Project/Scripts/Gameplay/Components/SpawnObject.cs
+++ Assets/_Project/Scripts/Gameplay/Components/SpawnObject.cs
@@ -1,5 +1,7 @@
+using System;
 using MonkeyBusiness.Utility;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace MonkeyBusiness.Gameplay.Components
 {
@@ -9,11 +11,25 @@ namespace MonkeyBusiness.Gameplay.Components
 
         [SerializeField] private GameObject prefab;
         [SerializeField] private Optional<Transform> parent;
+
+        [SerializeField] private Vector2 timeInterval = new(8, 15);
         
         #endregion
 
         private GameObject _spawnedInstance;
-        
+        private float _timerCountdown;
+
+        public void Update()
+        {
+            _timerCountdown -= Time.deltaTime;
+
+            if (_timerCountdown < 0)
+            {
+                Spawn();
+                _timerCountdown = Random.Range(timeInterval.x, timeInterval.y);
+            }
+        }
+
         public void Spawn()
         {
             _spawnedInstance = Instantiate(prefab, transform.position, transform.rotation, parent.Enabled ? parent : null);
